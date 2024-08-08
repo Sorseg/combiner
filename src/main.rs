863dcf528f785ce540bdcde5710e120281ee02be
sorseg <sorsegg@gmail.com>
2024-08-08 21:29:05 +0300

    Fix audio streams

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::time::Duration;
 
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    StreamConfig,
+};
 use crossbeam_channel::Receiver;
 use fundsp::hacker::{resonator_hz, An, AudioNode, AudioUnit, BlockRateAdapter, Frame, U0, U1};
 
@@ -29,57 +32,70 @@ impl AudioNode for InputNode {
 
 fn main() {
     let host = cpal::default_host();
-    let device = host.default_input_device().unwrap();
+    let (sound_sender, sound_receiver) = crossbeam_channel::bounded(4096);
+    let _rec_stream = {
+        let device = host.default_input_device().unwrap();
 
-    let config = device
-        .default_input_config()
-        .expect("Failed to get default input config");
+        let config = device
+            .default_input_config()
+            .expect("Failed to get default input config");
 
-    let (sound_sender, sound_receiver) = crossbeam_channel::unbounded();
+        println!("Record config on {} {config:?}", device.name().unwrap());
 
-    let _stream = device
-        .build_input_stream(
-            &config.into(),
-            move |data: &[f32], _| {
-                for v in data {
-                    sound_sender.send(*v).unwrap();
-                }
-            },
-            move |err| {
-                eprintln!("an error occurred on stream: {}", err);
-            },
-            None,
-        )
-        .unwrap();
+        let stream = device
+            .build_input_stream(
+                &config.into(),
+                move |data: &[f32], _| {
+                    for v in data {
+                        sound_sender.send(*v).unwrap();
+                    }
+                },
+                move |err| {
+                    eprintln!("an error occurred on stream: {}", err);
+                },
+                None,
+            )
+            .unwrap();
 
-    let playback_device = host.default_output_device().unwrap();
-    let config = playback_device.default_output_config().unwrap();
+        stream.play().unwrap();
+        stream
+    };
+    let _playback_stream = {
+        let playback_device = host.default_output_device().unwrap();
+        let config: StreamConfig = playback_device.default_output_config().unwrap().into();
+        println!(
+            "Playback config on {} {config:?}",
+            playback_device.name().unwrap()
+        );
 
-    let input = An(InputNode {
-        receiver: sound_receiver,
-    });
+        let input = An(InputNode {
+            receiver: sound_receiver,
+        });
 
-    let filter = resonator_hz(1700.0, 1300.0);
+        let filter = resonator_hz(1700.0, 1300.0);
 
-    let graph = input >> filter;
-    let mut graph = BlockRateAdapter::new(Box::new(graph));
-    graph.set_sample_rate(config.sample_rate().0 as f64);
+        let graph = input >> filter;
+        let mut graph = BlockRateAdapter::new(Box::new(graph));
+        graph.set_sample_rate(config.sample_rate.0 as f64);
 
-    let _stream = device
-        .build_output_stream(
-            &config.into(),
-            move |data: &mut [f32], _| {
-                for e in data {
-                    *e = graph.get_mono();
-                }
-            },
-            move |err| {
-                eprintln!("an error occurred on stream: {}", err);
-            },
-            None,
-        )
-        .unwrap();
-
+        let stream = playback_device
+            .build_output_stream(
+                &config,
+                move |data: &mut [f32], _| {
+                    for i in 0..data.len() / 2 {
+                        data[i * 2] = graph.get_mono();
+                        data[i * 2 + 1] = graph.get_mono();
+                    }
+                },
+                move |err| {
+                    eprintln!("an error occurred on stream: {}", err);
+                },
+                None,
+            )
+            .unwrap();
+        stream.play().unwrap();
+        stream
+    };
     loop {
         std::thread::sleep(Duration::from_secs(1))
     }
